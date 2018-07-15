1fd3f5eb0fe99f8e9ae978ea422bab0590ddb12d
tom <caolvchong@gmail.com>
2018-07-16 01:07:32 +0800

    增加使用自定义代码骨架能力

--- scripts/choice/classic.js
+++ scripts/choice/classic.js
@@ -1,28 +1,32 @@
 const { CLASSIC_REPO, CUSTOM } = require('../../constant/core')
-const { getUIName } = require('../../utils/project/sdp')
+const { getUIName } = require('gem-mine-helper')
 
 module.exports = function (context) {
+  const { ui } = context
+  const choices = [
+    {
+      name: '自定义代码骨架',
+      value: CUSTOM
+    },
+    {
+      name: '不，谢谢',
+      value: false
+    }
+  ]
+  if (ui) {
+    choices.unshift({
+      name: '使用管理后台代码骨架',
+      value: {
+        git: CLASSIC_REPO.ADMIN,
+        branch: getUIName(ui)
+      }
+    })
+  }
   return {
     type: 'list',
     name: 'classic',
     message: '是否使用经典代码骨架:',
-    choices: [
-      {
-        name: '使用管理后台代码骨架',
-        value: {
-          git: CLASSIC_REPO.ADMIN,
-          branch: getUIName(context.ui)
-        }
-      },
-      {
-        name: '自定义代码骨架',
-        value: CUSTOM
-      },
-      {
-        name: '不，谢谢',
-        value: false
-      }
-    ],
+    choices,
     default: false
   }
 }
