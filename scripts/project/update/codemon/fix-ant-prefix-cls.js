028ecb99cc3d2c64cc530ed414ff807331f6f189
tom <caolvchong@gmail.com>
2019-03-25 17:43:05 +0800

    codemon:增加jsx文件处理样式

--- scripts/project/update/codemon/fix-ant-prefix-cls.js
+++ scripts/project/update/codemon/fix-ant-prefix-cls.js
@@ -3,16 +3,16 @@ const fs = require('fs-extra')
 const { log } = require('@gem-mine/sapphire-helper')
 
 module.exports = function (root) {
-  log.info('将 css/less/sass 文件中的样式 .ant- 前缀 修改为 .fish- ')
-  rd.eachFilterSync(`${root}/src`, /\.(?:css|less|scss)$/, function (file) {
+  log.info('将 css/less/sass 文件中的样式 ant- 前缀 修改为 fish- ')
+  rd.eachFilterSync(`${root}/src`, /\.(?:css|less|scss|jsx?)$/, function (file) {
     transform(file)
   })
 }
 
 function transform(file) {
   let content = fs.readFileSync(file).toString()
-  if (content.indexOf('.ant-') > -1) {
-    content = content.replace(/\.ant-/g, '.fish-')
+  if (content.indexOf('ant-') > -1) {
+    content = content.replace(/ant-/g, 'fish-')
     fs.writeFileSync(file, content)
     log.info(`\t 处理文件 ${file} 成功`)
   }
